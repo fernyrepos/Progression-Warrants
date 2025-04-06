c615282894b1a18fbc42ab0ffbe9de8081c2902c
ferny <116464667+fernyrepos@users.noreply.github.com>
2025-04-06 06:50:57 -0400

    Welcome to git

---
+++ 1.5/Source/LockWarrantsBehindResearchMod.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace LockWarrantsBehindResearch
+{
+    [DefOf]
+    public static class Lock_DefOf
+    {
+        public static ResearchProjectDef WarrantNetwork;
+    }
+
+    public class MainButtonWorker_Warrants : MainButtonWorker_ToggleTab
+    {
+        public override bool Visible => base.Visible && Lock_DefOf.WarrantNetwork.IsFinished;
+    }
+
+    public class LockWarrantsBehindResearchMod : Mod
+    {
+        public LockWarrantsBehindResearchMod(ModContentPack pack) : base(pack)
+        {
+            new Harmony("LockWarrantsBehindResearchMod").PatchAll();
+        }
+    }
+}
